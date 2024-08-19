ade5ebe8d0b4c70919b0c1217898a6ed1afa5443
daihaa <dai293720@gmail.com>
2024-08-19 13:12:21 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
 })
 
 app.use(cors())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json())
 app.use(cookieParser());
 
@@ -31,5 +31,5 @@ mongoose.connect(`${process.env.MONGGO_DB}`)
     })
 
 app.listen(port, () => {
-    console.log('Server is running in port:', + port) 
+    console.log('Server is running in port:', + port)
 })
